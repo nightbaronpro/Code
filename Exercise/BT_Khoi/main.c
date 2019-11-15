0086bb6aa687115b765d977e857d9309256051dd
CaptainJAV <nvatuan0706@gmail.com>
2019-11-16 00:01:00 +0700

    Example2

---
+++ Exercise/BT_Khoi/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int n;
+    printf ("Nhap vao n: ");
+    scanf ("%d",&n);
+    if (n<2)
+    {
+        printf("Nhap sai!");
+    }
+    int dem = 0;
+    for (int i=2; i<sqrt(n); i++)
+    {
+        if (n%i == 0)
+        {
+            dem ++;
+        }
+    }
+    if (dem == 0)
+    {
+        printf ("%d la so nguyen to",n);
+    }
+    else 
+    {
+        printf ("%d khong la so nguyen to",n);
+    }
+    return 0;
+}
