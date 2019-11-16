bf11136c03e679b16c185f2b090b90826b0a5f87
CaptainJAV <nvatuan0706@gmail.com>
2019-11-16 16:39:45 +0700

    update

--- Exercise/Tach_Mang/main.c
+++ Exercise/Tach_Mang/main.c
@@ -19,34 +19,25 @@ void XuatMang (int a[],int n)
 }
 
 // Bai toan tach phan tu la so nguyen to trong mang a vao mang b va mang c la cac so con lai.
-int KiemTraNguyenTo(int n)
+int checkPrimeNumber(int n)
 {
-	if (n < 2)
-	{
-		return 0;
-	}
-	else if (n > 2)
-	{
-		if (n % 2 == 0)
-		{
-			return 0;
-		}
-		for (int i = 3; i <= sqrt((float)n); i += 2)
-		{
-			if (n % i == 0)
-			{
-				return 0;
-			}
-		}
-	}
-	return 1;
+    int i, flag = 1;
+    for(i=2; i <= n/2; ++i)
+    {
+        if (n%i == 0)
+        {
+            flag =0;
+            break;
+        }
+    }
+    return flag;
 }
 void TachMang (int a[],int n)
 {
     int b[MAX], nb = 0;
     for (int i=0; i<n; i++)
     {
-        if (KiemTraNguyenTo(a[i]) == 1)
+        if (checkPrimeNumber(a[i]) == 1)
         {
             b[nb] = a[i];
             nb++;
