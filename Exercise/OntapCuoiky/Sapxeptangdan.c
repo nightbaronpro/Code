bd7afa46dae2a894d719d69d177fe56f3aea4320
CaptainJAV <nvatuan0706@gmail.com>
2019-11-23 00:26:31 +0700

    update

---
+++ Exercise/OntapCuoiky/Sapxeptangdan.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<math.h>
+#define MAX 100
+
+void nhap (int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		printf("\nNhap a[%d]: ", i);
+		scanf("%d", &a[i]);
+	}
+}
+
+void xuat(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		printf("%d\t", a[i]);
+	}
+}
+
+void saptang(int a[], int n)
+{
+	for(int i = 0; i < n - 1; i++)
+	{
+		for(int j = i + 1; j < n; j++)
+		{
+			if(a[i] > a[j])
+			{
+				float temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+		}
+	}
+}
+int main()
+{
+	int n;
+	int a[MAX];
+
+	do
+	{
+		printf("\nNhap so phan tu: ");
+		scanf("%d", &n);
+		if(n <= 0 || n > MAX)
+		{
+			printf("\nSo phan tu khong hop le. Xin kiem tra lai !");
+		}
+	}while(n <= 0 || n > MAX);
+
+	nhap(a, n);
+	xuat(a, n);
+
+	saptang(a, n);
+	printf("\nMang sau khi sap xep tang: ");
+	xuat(a, n);
+
+	return 0;
+}
